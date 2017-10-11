73f3e3a0f65cf10cbb3bba56d7f1ba0114608b57
briandyko <briandyko@gmail.com>
2017-10-11 11:16:09 -0400

    added properties and fields to vehicle

---
+++ Week 3_Day 3_Polymorphism/Vehicle.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_3_Day_3_Polymorphism
+{
+    class Vehicle
+    {
+        private int seats;
+        private int carryingCapacity;
+        private string color;
+        private double movementSpeed;
+        private float gearRatio;
+        private float engineSize;
+
+        public int Seats
+        {
+            get { return this.seats; }
+        }
+
+        public int CarryingCapacity
+        {
+            get { return this.carryingCapacity; }
+        }
+
+        public string Color
+        {
+            get { return this.color; }
+            set { this.color = value; }
+
+
+        }
+
+    }
+}
